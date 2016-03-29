f77281d30ff4cd4aaa4ef49822c44808a321b76e
mk-initflow <maksim.k@initflow.com>
2016-03-29 13:23:50 +0300

    Move markup here

---
+++ app/js/script.js
@@ -0,0 +1,192 @@
+/* ###### init skrol to point  ######*/
+/* ###### bower i page-scroll-to-id  ######*/
+(function($){
+    $(window).load(function(){
+        $("a[rel='m_PageScroll2id']").mPageScroll2id({
+				  highlightClass:"left-nav-el-active"
+				});
+				 $("a[rel='m_PageScroll2id']").mPageScroll2id({
+				  highlightClass:"left-nav-el-active"
+				});
+    });
+ })(jQuery); 
+
+$(document).ready(function(){
+
+	$('[data-open-callback-modal]').on('click', function(){
+		$('.callback-modal').bPopup();
+	});
+
+
+	//modal
+	$('.section-main-form button').click(function(){
+		$('.section-modal').bPopup({
+	 			closeClass:'section-modal-but',
+				position:['auto','auto'],
+				follow: [true,false],
+	 	}); 
+	})
+
+
+	$('.section-view').css('height',$(window).height())
+	$(window).resize(function(){
+		$('.section-view').css('height',$(window).height())
+	});
+	console.log($(window).height()	)
+	console.log($(window).width()	)
+	var numberUserBlock = function(selector){
+		var $elem = $(selector),
+			number = parseInt($elem.text()),
+			current = number - 176,
+			length = number.toString().length,
+			interval = null, 
+			additive = 1,
+			delay = 15,
+			numbers = $.find();
+
+
+		$elem.empty();
+
+		for(var i = 0; i < length; i++){
+			var $newNumber = $('<span class="section-main-title-number-el">0</span>');
+			numbers.push($newNumber);
+			$elem.append($newNumber);
+		}
+
+		interval = setInterval(function(){
+			current+= additive;
+			if(current >= number){
+				current = number;
+				clearInterval(interval);
+			}
+			showNumber();
+		}, delay);
+
+		function showNumber(){
+			var newText = '0000000000000000000' + current;
+			newText = newText.substr(newText.length - length);
+			var newtextItems = newText.split('');
+			for(var sI = 0; sI < length; sI++){
+				numbers[sI].text(newtextItems[sI]);
+			}
+		}
+	}
+
+numberUserBlock('.section-main-title-number');
+numberUserBlock('.section-about-number');
+
+	$('.section-main-video').on('click', function(ev) {
+ 
+    $(".introVideo")[0].src += "&autoplay=1";
+    ev.preventDefault();
+ 		$('.section-main-video img').hide();
+ 		$('.section-main-video-filter').hide();
+  });
+
+	//animate logo
+	$(window).scroll(function(){
+		var needUpSpace = 800;
+		var upSpace = $(this).scrollTop();
+		var a = $(".top-menu-logo").css('width')
+		if ( upSpace >= 800 && a == "0px") {$(".top-menu-logo").stop().animate({'width':'200'},500)};
+		if ( upSpace < 800 && a == "200px") {$(".top-menu-logo").stop().animate({'width':'0'},500)};
+	})
+
+	//Tooltip
+	$('.section-time-quote-bottom-link').click(function(){
+		$('.section-time-quote-tooltip-wraper').toggleClass('hidden');
+	})
+	/* ###### init bpopup  ######*/
+	/* ###### bower i bpopup  ######*/
+	// $('.button-modal').click(function(){
+	// 	$('.modal-block').bPopup({
+	// 			closeClass:'closer'
+	// 	}); 
+	// })
+	/* ###### init stickUp  ######*/
+	/* ###### bower i sticky  ######*/
+	/*$("#sticker").sticky({topSpacing:0});*/
+
+
+	/* ###### init OwlCarousel2  ######*/
+	/*!!! add class owlCarousel !!!*/
+	/* ###### bower i OwlCarousel2 ######*/
+	$(".section-about-slider").owlCarousel({
+	 	center: true,
+    items:1,
+	 	autoHeight : true,
+	 	pagination : true,
+	 	autoPlay : true,
+	 	nav:true,
+			navText:['<img src="img/nav-left.png">','<img src="img/nav-right.png">']
+	 	}
+	 ); 	
+
+
+	$(".section-tarifs-slider").owlCarousel({
+	 	center: true,
+    items:1,
+	 	autoHeight : true,
+	 	pagination : true,
+	 	autoPlay : true,
+	 	nav:true,
+			navText:['<img src="img/nav-left.png">','<img src="img/nav-right.png">']
+	 	}
+	 ); 
+
+
+	/* ###### init validate form  ######*/
+	/* ###### bower i jquery-validation ######*/
+	/*$('#myform').validate({
+			rules:{ //правила для полей 
+				name:{
+					required:true,
+					minlength:5 //минимальное значение поля
+				},
+				phone:{
+					required:true,
+					number:true
+				}
+			},
+			messages:{
+				name:{
+					required: 'Это поле обязатлеьно для заполнения', //какое сообщение будет выводиться
+					minlength:'Имя должно быть не меньше 5 символов'
+				},
+				phone:{
+					required: 'Это поле обязатлеьно для заполнения',
+					number:'Введите правильный телефон'
+				},
+				
+			}
+			submitHandler:function(){ //выполнять если все валидно
+					alert('Форма заполнена правильно');
+				}
+	})*/
+
+	/* ###### init animatedModal  ######*/
+	/* ###### bower i animatedModal  ######*/
+	// $(".play").animatedModal({
+	//  	 animatedIn:'lightSpeedIn',
+	//     animatedOut:'bounceOutDown',
+	//     color:'#0394c7'
+	//  	});
+
+	/* ###### init responsive-tabs  ######*/
+	/* ###### bower i responsive-tabs  ######*/
+/*    $('#horizontalTab').responsiveTabs({
+        rotate: false,
+        startCollapsed: 'accordion',
+        collapsible: 'accordion',
+        setHash: true,
+        active: 0
+        
+    });*/
+
+	/* ###### init fancybox  ######*/
+	/* ###### bower i fancybox  ######*/
+	// $(".play").fancybox();
+	// a(href="img/item-house-1.png" rel="group-element(для объединения в группу)") - image in a
+	//	img(src="img/item-house-1.png", alt="")
+	
+})
---
+++ gulpfile.js
@@ -0,0 +1,235 @@
+var ftpConf = {
+  "user":"",
+  "pass":"",
+  "name":"catalog"
+}
+
+var gulp = require("gulp");
+var autoprefixer = require('gulp-autoprefixer');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var stylus = require('gulp-stylus');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var ftp = require( 'vinyl-ftp' );
+var reload = browserSync.reload;
+var fileinclude = require('gulp-file-include');
+var axis = require('axis');
+var jeet = require('jeet');
+var htmlhint = require("gulp-htmlhint");
+var rupture = require('rupture');
+var data = require('gulp-data');
+var jade = require('gulp-jade');
+var notify = require('gulp-notify');
+var pngquant = require('imagemin-pngquant');
+var imageminMozjpeg = require('imagemin-mozjpeg');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cached');
+var newer = require('gulp-newer');
+var sprity = require('sprity');
+
+
+// ########## make img ###############
+
+// generate sprite.png and _sprite.scss 
+gulp.task('sprites', function () {
+  return sprity.src({
+    src: 'app/img/sprite/*.png',
+    style: '_sprite.styl',
+     cssPath: './img/sprite/',
+     processor: 'slylus'
+  })
+  .pipe(gulpif('*.png', gulp.dest('app/img/'), gulp.dest('app/css/')))
+});
+
+
+gulp.task('imagePng',function(){
+ return gulp.src('app/img/bg-section-main.png')
+     //.pipe(newer('dist/img/'))
+     .pipe(imagemin({
+         progressive: true,
+         svgoPlugins: [{removeViewBox: false}],
+         use: [pngquant({quality: '70', speed: 4})]
+     }))
+     .pipe(gulp.dest('dist/img/'));
+ });
+
+
+gulp.task('imageJpg',function(){
+  return gulp.src('app/img/*.jpg')
+  .pipe(newer('dist/img/'))
+  .pipe(imagemin({
+          progressive: true,
+          svgoPlugins: [{removeViewBox: false}],
+          use: [imageminMozjpeg({quality: '50', speed: 11})]
+      }))
+  .pipe(gulp.dest('dist/img/'));
+});
+
+// ########## make css ###############
+
+//Prefix my css
+gulp.task('prefix', function () {
+    return gulp.src('app/css/style.css')
+        .pipe(autoprefixer({
+            browsers: ['last 15 versions']
+        }))
+        .pipe(gulp.dest('app/css/'));
+});
+
+//Stylus
+gulp.task('stylus', function () {
+  return gulp.src('./app/css/*.styl')
+    .pipe(cache('stylus'))
+    .pipe(stylus({
+        use:[rupture(),axis(),jeet()]
+        })).on('error', errorhandler)
+    .pipe(gulp.dest('./app/css/'))
+  
+});
+
+//Source map
+gulp.task('sourcemaps', function () {
+  return gulp.src('./app/css/style.styl')
+    .pipe(sourcemaps.init())
+    .pipe(stylus({
+        use:[rupture(),axis(),jeet()]
+        }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./app/css/'))
+});
+
+// ########## make css end ###############
+
+// ########## make html ###############
+
+//Include html
+gulp.task('fileinclude', function() {
+  gulp.src('./app/html/*.html')
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: './app/html/block_html/'
+    }))
+    .pipe(gulp.dest('./app/'));
+});
+
+//Jade
+gulp.task('jade', function() {
+  return gulp.src('./app/html/**/**.jade')
+    .pipe(cache('jade'))
+    .pipe(data( function(file) {
+            return require('./app/html/data.json');
+    } ))
+    .pipe(jade({
+      pretty: true
+    }).on('error', errorhandler))
+    .pipe(gulp.dest('./app/'))
+});
+
+gulp.task('include',function(){
+        gulp.watch('app/html/**/*.html',['fileinclude'])
+})
+// ########## make html end###############
+
+
+// ########## make service###############
+//copy file
+gulp.task('copy:font',function(){
+  return gulp.src('./app/fonts/**.*')
+         .pipe(gulp.dest('./dist/fonts/'))
+  })
+
+gulp.task('copy:js',function(){
+  return gulp.src('./app/js/script.js')
+         .pipe(gulp.dest('./dist/js/'))
+  })
+
+//Show error
+function errorhandler(a) {
+    var args = Array.prototype.slice.call(arguments);
+    // Send error to notification center with gulp-notify
+    notify.onError({
+        title: 'Compile Error',
+        message: a,
+        // sound: true можно даже со звуком!
+    }).apply(this, args);
+
+    // Keep gulp from hanging on this task
+    this.emit('end');
+};
+
+//useref
+gulp.task('make', function () {
+  var assets = useref.assets();
+   gulp.src('app/js/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/js/'));
+
+   gulp.src('app/css/style.css')
+  .pipe(minifyCss())
+  .pipe(gulp.dest('dist/css/'));
+  return gulp.src('app/*.html')
+      .pipe(assets)
+      .pipe(gulpif('*.js', uglify()))
+      .pipe(gulpif('*.css', minifyCss()))
+      .pipe(assets.restore())
+      .pipe(useref())
+      .pipe(gulp.dest('dist'));
+});
+
+//Ftp
+gulp.task( 'ftp', function() {
+    var conn = ftp.create( {
+        host:     'one.web-kuznetcov.ru',
+        user:     ftpConf.user,
+        password: ftpConf.pass,
+        parallel: 21,
+    } );
+    var globs = [
+        'dist/**',
+        'dist/*.html'
+    ];
+   return gulp.src(globs)
+        .pipe( conn.newer( 'httpdocs/one.web-kuznetcov.ru/'+ftpConf.name) )
+        .pipe( conn.dest( 'httpdocs/one.web-kuznetcov.ru/'+ftpConf.name) );
+
+} );
+
+//LiveReload
+gulp.task('serve', function () {
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "./app/",
+        }
+    });
+    browserSync.watch(["./app/css/**/*.css","./app/*.html","./app/js/**.*"]).on("change", browserSync.reload);
+});
+
+// ########## make service end ###############
+
+//Watcher
+gulp.task('see',function(){
+        gulp.watch('app/html/**/*.jade',['jade'])
+        gulp.watch('app/css/*.styl',['stylus'])
+})
+
+//default
+gulp.task('img',['imagePng' , 'imageJpg']);
+gulp.task('default', ['serve','see']);
+gulp.task('build',['sourcemaps','copy:font','prefix','make']);
+gulp.task('fast-build',['stylus','prefix','jade','copy:js','ftp']);
+
+
+
+gulp.task('fast-see',function(){
+        gulp.watch(["./app/css/**/**.styl","./app/html/**.jade","./app/js/**.*"] ,gulp.series('fast-build'))
+})
+
+
+//gulp.task('default', function() {
+//  runSequence('see');
+//});
+
